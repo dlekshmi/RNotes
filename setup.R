00cbafa77a0d76ea46b29ff4ade88572290b1b6d
dlekshmi <lekshmi.dharmarajan@gmail.com>
2020-07-18 17:57:35 +0200

    Changed checkpoint linrary addition

--- setup.R
+++ setup.R
@@ -1,6 +1,6 @@
 if (!require("checkpoint", character.only = TRUE)) {
   install.packages("checkpoint", dependencies = TRUE)
-  library(checkpoint, character.only = TRUE)
+  library(checkpoint)
 }
 checkpoint('2020-07-17')
 
