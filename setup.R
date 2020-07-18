83c70043c57cbcee0434d05029bd6724568cb295
dlekshmi <lekshmi.dharmarajan@gmail.com>
2020-07-18 17:51:55 +0200

    modified setup:

--- setup.R
+++ setup.R
@@ -1,5 +1,5 @@
 if (!require("checkpoint", character.only = TRUE)) {
-  install.packages(checkpoint, dependencies = TRUE)
+  install.packages("checkpoint", dependencies = TRUE)
   library(checkpoint, character.only = TRUE)
 }
 checkpoint('2020-07-17')
